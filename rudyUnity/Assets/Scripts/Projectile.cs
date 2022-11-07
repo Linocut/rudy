6eab6f6a26ae2222282e3fe11ff31bcc361e98aa
Jasmine May Darman <Jelloismynick@gmail.com>
2022-11-06 19:08:57 -0500

    logout

--- rudyUnity/Assets/Scripts/Projectile.cs
+++ rudyUnity/Assets/Scripts/Projectile.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,18 +6,32 @@ public class Projectile : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     
-    void Start()
+    void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
+    
     public void Launch(Vector2 direction, float force)
     {
         rigidbody2d.AddForce(direction * force);
     }
+    
+    void Update()
+    {
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
     void OnCollisionEnter2D(Collision2D other)
     {
-        //we also add a debug log to know what the projectile touch
-        Debug.Log("Projectile Collision with " + other.gameObject);
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+    
         Destroy(gameObject);
     }
 }
